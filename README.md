6e8f86f8ea91384f054047de1a716e5cec22fa6f
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-02-18 09:48:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot3top20251
---
+++ src/main/java/br/gov/sp/fatec/springboot3top20251/Springboot3top20251Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3top20251;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3top20251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3top20251Application.class, args);
+	}
+
+}
