b6e05c7e7acab1ef394047a115c5e6847876bdba
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-05-26 08:32:11 -0300

    Consertando o DDL

--- DDL.sql
+++ DDL.sql
@@ -33,7 +33,7 @@ create table ant_anotacao (
 create table tra_trabalho (
   tra_id bigint generated always as identity,
   tra_titulo varchar(100) not null unique,
-  tra_data_hora_entrega timestamp not null,
+  tra_data_hora_entrega timestamp,
   tra_descricao varchar(200),
   tra_grupo varchar(20) not null,
   tra_nota int,
@@ -50,7 +50,7 @@ insert into ant_anotacao(ant_texto, ant_data_hora, ant_usr_id)
   values('Meu novo projeto', '2023-08-01 19:10', 1);
 insert into tra_trabalho (tra_titulo, tra_data_hora_entrega, tra_grupo, tra_nota, tra_justificativa)
   values ('Teste 1', current_timestamp, 'Alpha', 6, 'Bom, mas falta conteúdo'),
-         ('Teste 2', current_timestamp, 'Beta', null, 'Incompleto');
+         ('Teste 2', null, 'Beta', null, 'Incompleto');
 
 
 create user spring with password 'pass123';
